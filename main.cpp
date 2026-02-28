e44ad7b581319d54e18e8de9fd839073483951cd
RubBarkhudaryan <rubenbarkhudaryan2005@gmail.com>
2026-03-01 03:29:15 +0400

    some readability improvement changes

--- main.cpp
+++ main.cpp
@@ -9,28 +9,33 @@ int	main(int argc, char **argv)
 	if (argc == 3)
 	{
 		Chessboard	board = parse_config(argv[1]);
+
 		if (board.empty())
 		{
 			print_error(RED, EMPTY_BOARD_ERR);
 			return (1);
 		}
+
 		board.display();
+
 		Engine				engine(board);
 		int					depth = std::stoi(argv[2]);
 		std::vector<Move>	line;
-		Logger				log;
 
 		if (engine.is_king_in_check(Color::WHITE) && engine.is_king_in_check(Color::BLACK))
 		{
 			print_error(RED, KINGS_IN_CHECK);
 			return (1);
 		}
+
 		if (engine.mate_in_n(Color::WHITE, Color::WHITE, depth * 2, line))
 		{
+			Logger	log;
 			std::cout << "White wins" << std::endl;
 			std::cout << "You can see the logfile with every step in '/logs' directory." << std::endl;
 			log.create_log(board, line);
 		}
+
 		else
 			std::cout << "White doesn't mate in " << depth << std::endl;
 	}
